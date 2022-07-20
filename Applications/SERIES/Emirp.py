449748cfbf2ed8ef7fdc8246706a17f9872401b0
praabindhp <praabindhp@gmail.com>
2022-07-20 19:32:32 +0530

    Progressing

---
+++ Applications/SERIES/Emirp.py
@@ -0,0 +1,33 @@
+def reverse(x):
+     
+    rev = 0;
+    while (x > 0):
+        rev = (rev * 10) + x % 10;
+        x = int(x / 10);
+ 
+    return rev;
+ 
+def printEmirp(n):
+    prime = [1] * (n + 1);
+    p = 2;
+    while (p * p <= n):
+         
+        if (prime[p] == 1):
+             
+            for i in range(p * 2, n + 1, p):
+                prime[i] = 0;
+        p += 1;
+
+    for p in range(2, n + 1):
+        if (prime[p] == 1):
+             
+            rev = reverse(p);
+ 
+            if (p != rev and rev <= n and
+                       prime[rev] == 1):
+                print(p, rev, end = " ");
+
+                prime[rev] = 0;
+
+n = 100;
+printEmirp(n);
