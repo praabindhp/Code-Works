1861396196bb87a4c46acf877d21606c0659a882
praabindhp <praabindhp@gmail.com>
2022-10-06 11:15:44 +0530

    Progressing

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, request, render_template
+import random
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route('/', methods =["GET", "POST"])
-def gfg():
-	if request.method == "POST":
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-	    # first_name = request.form.get("fname")
-	    # last_name = request.form.get("lname")
-	
-	    return render_template("index.html")
+@app.route('/', methods=["POST"])
+def get_value():
+    n=request.form["count"]
+    num=int(n)
+
+    cnl=request.form["objects"]
+    listOriginal=cnl.split()
+
+    listRandom=random.sample(listOriginal, num)
+    print(listRandom)
+    return render_template('results.html', randomlist=listRandom, num=num)
 
 if __name__=='__main__':
+    app.debug=True
+    app.run()
     app.run(debug=True)
